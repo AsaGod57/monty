c6024599bfd96c16b5f007126491354f60e46120
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-17 18:04:28 -0800

    My mqueue commit

---
+++ mqueue.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ * f_queue - Starts to print the top
+ * @head: Specifie stack head
+ * @counter: Specifies line_number
+ * Return: The no return
+*/
+
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - Starts to add node to the tail stack
+ * @n: Specifies new_value
+ * @head: Specifies head of the stack
+ * Return: The no return (Ends)
+*/
+
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
