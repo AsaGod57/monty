e23f5697f4b25aa82d0ee32fe032448c766220a9
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-17 18:13:21 -0800

    My mstack commit

---
+++ mstack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - Start to print the top
+ * @head: Specifies stack head
+ * @counter: Specifies line_number
+ * Return: The no return (Ends)
+*/
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
