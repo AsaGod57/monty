33db84a249896d4f5fb510f0b2eb840f63ed53eb
Godsway <gasamoah@wacci.ug.edu.gh>
2024-01-17 17:38:12 -0800

    My mnop commit

---
+++ mnop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  *f_nop- Start nothing
+  *@head: Specifies stack head
+  *@counter: Specifies line_number
+  *Return: The no return (Ends)
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
